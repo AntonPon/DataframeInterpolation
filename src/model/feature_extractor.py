09765ae4668a686e1ece149d3d27d1b5adaedf41
AntonPon <ponomarchuk.anton@gmail.com>
2018-06-02 00:12:54 +0300

    added feature extractor

---
+++ src/model/feature_extractor.py
@@ -0,0 +1,23 @@
+from src.model.youtube_8m.feature_extractor import feature_extractor
+import pickle
+import os
+
+def get_features(image):
+    return feature_extractor(image)
+
+
+path = '../../../dataset'
+print(os.listdir(path))
+
+files = os.listdir(path)
+file_path = os.path.join(path, files[0])
+print(file_path)
+
+
+def extract_features(image):
+    extracor = feature_extractor.YouTube8MFeatureExtractor()
+    return extracor.extract_rgb_frame_features(image)
+
+with open(file_path, 'rb') as f:
+    p = pickle.load(f)
+    print(extract_features(p[0]))
